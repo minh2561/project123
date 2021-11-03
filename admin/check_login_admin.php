4926c4009248b918bbfaab31428a468db2c651f8
minh2561 <84631891+minh2561@users.noreply.github.com>
2021-11-04 00:06:29 +0700

    Delete admin directory

--- admin/check_login_admin.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    if(!isset($_SESSION['admin'])) 
-    {
-        $_SESSION['no-login-message'] = "<div class='error text-center'>Please login to access </div>";
-        header('location:'.SITEURL.'index/index_admin.php');
-    }
-
-    ?>
